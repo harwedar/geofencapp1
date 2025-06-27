4944e89e5261dc3c60733e05aa7fd64d9cf3650a
SynteQ <76913773+harwedar@users.noreply.github.com>
2025-06-27 19:19:27 +0100

    Update seed.js

--- seed.js
+++ seed.js
@@ -1,11 +1,14 @@
 
 // seed.js
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://awedaolusina:synteqdeveloper@cluster0.prmgr.mongodb.net/geofence?retryWrites=true&w=majority&appName=Cluster0');
-', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  'mongodb+srv://awedaolusina:synteqdeveloper@cluster0.prmgr.mongodb.net/geofence?retryWrites=true&w=majority&appName=Cluster0',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
 
 const AdminUser = mongoose.model('AdminUser', new mongoose.Schema({
   username: String,
