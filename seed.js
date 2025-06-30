b16132ee51bb394015ebf920542ad13bf4d7099e
SynteQ <76913773+harwedar@users.noreply.github.com>
2025-06-30 16:30:16 +0100

    Update seed.js

--- seed.js
+++ seed.js
@@ -2,13 +2,14 @@
 // seed.js
 const mongoose = require('mongoose');
 mongoose.connect(
-  'mongodb+srv://awedaolusina:synteqdeveloper@cluster0.prmgr.mongodb.net/geofence?retryWrites=true&w=majority&appName=Cluster0',
+  'mongodb+srv://SynteQ Technologies:developerSYNQ76@cluster0.aahlm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }
 );
 
+//mongodb+srv://<db_username>:<db_password>@cluster0.aahlm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
 const AdminUser = mongoose.model('AdminUser', new mongoose.Schema({
   username: String,
