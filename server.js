2970ce5130210182437a10f19c53afd136f43bdb
SynteQ <76913773+harwedar@users.noreply.github.com>
2025-07-05 21:54:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.get('/', (req, res) => {
 });
 
 //app.listen(3000, () => console.log('Server running on http://localhost:3000'));
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
